a54160a50c8be9bb32690448fdccdf398e29299c
Salocin1987 <72270336+Salocin1987@users.noreply.github.com>
2020-10-02 19:53:09 +0200

    Add files via upload

---
+++ data/Photos_2.js
@@ -0,0 +1,14 @@
+var json_Photos_2 = {
+"type": "FeatureCollection",
+"name": "Photos_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -90.12378182350696, 29.931130496355237 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -90.122856123637135, 29.933506318896526 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -90.126843753845549, 29.933352046427618 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -90.129122399678906, 29.930050558267169 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -90.131401045512277, 29.927180951550564 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -90.128873172790904, 29.925823259325412 ] } },
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "Point", "coordinates": [ -90.12670133848097, 29.925267834441023 ] } }
+]
+}
